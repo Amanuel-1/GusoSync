a4938342166985b206f21473c95f2f3b22c75a1e
Amanuel Garomsa <71079278+Amanuel-1@users.noreply.github.com>
2025-04-16 01:36:04 +0300

    formatNumber

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function formatNumber(number:any, decimals:number = 0) {
+  return parseFloat(number).toFixed(decimals)
+}
+
+export function formatSpeed(speed:number) {
+  return `${speed} km/h`
+}
